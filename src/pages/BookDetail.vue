7e5f06d9c7b5916799974262b9545e6a0449c612
mukhammadyor17 <mukhammadyorkhon.t@evrika.com>
2024-02-07 11:54:58 +0600

    feat: add arabic fonts

--- src/pages/BookDetail.vue
+++ src/pages/BookDetail.vue
@@ -1,27 +1,34 @@
 <template>
   <q-page class="">
-
     <div class="flex justify-center q-py-md" v-if="loading">
-      <q-spinner-ios
-        color="white"
-        size="30px"
-      />
+      <q-spinner-ios color="white" size="30px" />
     </div>
-    
-    <div class="surah q-py-md text-white q-mx-auto" v-if="surah || !loading" style="max-width: 700px">
-      <div class="flex justify-between q-px-lg text-subtitle2 q-mb-md"> 
-        <p style="width: 50px">{{surah.number}}</p> 
-        <p>{{surah.asma.en.long}}</p> 
-        <p>{{surah.asma.ar.long}}</p> 
+
+    <div
+      class="surah q-py-md text-white q-mx-auto"
+      v-if="surah || !loading"
+      style="max-width: 700px"
+    >
+      <div class="flex justify-between q-px-lg text-subtitle2 q-mb-md">
+        <p style="width: 50px">{{ surah.number }}</p>
+        <p>{{ surah.asma.en.long }}</p>
+        <p>{{ surah.asma.ar.long }}</p>
       </div>
-      <p class="text-h6" v-if="surah.preBismillah"> {{surah.preBismillah.text.ar}} </p>
-      <p v-for="sura in surah.ayahs" :key="sura.number.inquran" class="text-h6 border-b q-px-md">
-        {{sura.text.ar}}<br>
-        <span class="text-caption">{{sura.number.insurah}}. </span> 
-        <span class="text-body1">{{sura.text.read}}</span>
+      <p class="verse-text q-mb-xl" v-if="surah.preBismillah">
+        {{ surah.preBismillah.text.ar }}
+      </p>
+      <p
+        v-for="sura in surah.ayahs"
+        :key="sura.number.inquran"
+        class="text-h6 border-b q-px-md verse-text"
+      >
+        {{ sura.text.ar }}<br />
+        <div>
+          <span class="text-caption">{{ sura.number.insurah }}. </span>
+          <span class="text-body1">{{ sura.text.read }}</span>
+        </div>
       </p>
     </div>
-
   </q-page>
 </template>
 
@@ -31,32 +38,40 @@ export default {
     return {
       surah: null,
       id: this.$route.params.id,
-      loading: true
-    }
+      loading: true,
+    };
   },
   created() {
-    if(this.id) {
-      this.getSurahById()
+    if (this.id) {
+      this.getSurahById();
     }
   },
   mounted() {},
   methods: {
     getSurahById() {
-      this.$axios.get(`https://quran-endpoint.vercel.app/quran/${this.id}`)
+      this.$axios
+        .get(`https://quran-endpoint.vercel.app/quran/${this.id}`)
         .then((res) => {
-          this.surah = res.data.data
-          this.loading = false
+          this.surah = res.data.data;
+          this.loading = false;
         })
-        .catch(e => {
+        .catch((e) => {
           console.log(e.message);
-        })
-    }
-  }
-}
+        });
+    },
+  },
+};
 </script>
 
 <style lang="scss" scoped>
 .surah {
   text-align: center;
+}
+
+.verse-text {
+  font-size: 28px;
+  font-weight: normal;
+  font-family: "Hafs", sans-serif;
+  // font-family: "Nabi", sans-serif;
 }
 </style>
