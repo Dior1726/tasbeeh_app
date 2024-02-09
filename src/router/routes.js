addefe1576890cdd498d134cad2ef877d8ee3bfb
mukhammadyor17 <mukhammadyorkhon.t@evrika.com>
2024-02-09 11:58:04 +0600

    refactor: routes.js

--- src/router/routes.js
+++ src/router/routes.js
@@ -79,9 +79,6 @@ const routes = [
       },
     ],
   },
-
-  // Always leave this as last one,
-  // but you can also remove it
   {
     path: "/:catchAll(.*)*",
     component: () => import("pages/ErrorNotFound.vue"),
